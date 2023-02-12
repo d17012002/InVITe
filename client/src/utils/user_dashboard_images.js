f5d32ff29f01d450c4439b7546aba6f93ca1f819
Saksham Gupta <saksham.gupta2020@vitbhopal.ac.in>
2023-02-12 23:05:46 +0530

    basic structure for user dashboard

---
+++ client/src/utils/user_dashboard_images.js
@@ -0,0 +1,41 @@
+
+const Images = [
+    {
+        id: 0,
+        src: "/img/user_dashboard_images/list1.png",
+        title: "photo1",
+        club: "xyz"
+    },
+    {
+        id: 1,
+        src: "/img/user_dashboard_images/list2.png",
+        title: "photo2",
+        club: "xyz"
+    },
+    {
+        id: 2,
+        src: "/img/user_dashboard_images/list3.png",
+        title: "photo3",
+        club: "xyz"
+    },
+    {
+        id: 3,
+        src: "/img/user_dashboard_images/list4.png",
+        title: "photo4",
+        club: "xyz"
+    },
+    {
+        id: 4,
+        src: "/img/user_dashboard_images/list5.png",
+        title: "photo5",
+        club: "xyz"
+    },
+    {
+        id: 5,
+        src: "/img/user_dashboard_images/list6.png",
+        title: "photo6",
+        club: "xyz"
+    }
+];
+
+export default Images;
