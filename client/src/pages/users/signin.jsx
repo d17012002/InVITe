c8dd46bc9d6ea5309069422310504c3eb5d690d5
Saksham Gupta <saksham.gupta2020@vitbhopal.ac.in>
2023-02-13 23:16:16 +0530

    added basic signin and signup facilities

--- client/src/pages/users/signin.jsx
+++ client/src/pages/users/signin.jsx
@@ -1,6 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function signin() {
+
+  const [email, setEmail] = useState('');
+  const [otp, setOtp] = useState('');
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/signin/verify`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          otp: otp
+        })
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleVerify = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/signin`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email
+        })
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
   return (
     <div className="m-2">
       {/* Back Button */}
@@ -14,12 +59,47 @@ export default function signin() {
       <div className="text-3xl font-bold">Signin Page</div>
 
       {/* Page Content */}
-      <div>Enter your phone number and press 'Send-OTP' button to verify</div>
-      <div>{"[ Input tag for phone number ] [ 'Send-OTP' Button]"}</div>
-      <br />
+      <div>
+        <form onSubmit={handleVerify} class="bg-white p-6 rounded-lg shadow-md">
+          <div class="mb-4">
+            <label class="block text-gray-700 font-medium mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              class="w-full border border-gray-400 p-2"
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
 
-      <div>Enter your OTP in the OTP field and press 'signin' button</div>
-      <div>{"[ Input tag for OTP ] [ 'Signin' Button (open user dashboard after verifying) ]"}</div>
+          <button class="bg-indigo-500 text-white py-2 px-4 rounded-full hover:bg-indigo-600">
+            Verify
+          </button>
+        </form>
+
+        <form onSubmit={handleSubmit} class="bg-white p-6 rounded-lg shadow-md">
+          <div class="mb-4">
+            <label class="block text-gray-700 font-medium mb-2" htmlFor="otp">
+              OTP
+            </label>
+            <input
+              class="w-full border border-gray-400 p-2"
+              type="text"
+              id="otp"
+              name="otp"
+              value={otp}
+              onChange={(event) => setOtp(event.target.value)}
+            />
+          </div>
+
+          <button class="bg-indigo-500 text-white py-2 px-4 rounded-full hover:bg-indigo-600">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
--- client/src/pages/users/signup.jsx
+++ client/src/pages/users/signup.jsx
@@ -1,6 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function signup() {
+
+  const [email, setEmail] = useState('');
+  const [otp, setOtp] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+  const [regNumber, setRegNumber] = useState('');
+  const [username, setUsername] = useState('');
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/signup/verify`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          contactNumber: contactNumber,
+          otp: otp,
+          email: email,
+          regNumber: regNumber,
+          username: username
+        })
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleVerify = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/signup`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email
+        })
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="m-2">
       {/* Back Button */}
@@ -14,21 +64,98 @@ export default function signup() {
       <div className="text-3xl font-bold">Signup Page</div>
 
       {/* Page Content */}
-      <div>First, you'll have to enter your phone number and press 'Send-OTP'</div>
-      <div>{"[ Input tag for Phone Number ] [ 'Send-OTP' Button ]"}</div>
-      <br />
-
-      <div>Enter your OTP in the OTP field and press 'Verify-OTP' button</div>
-      <div>{"[ Input tag for OTP ] [ 'Verify-OTP' Button ]"}</div>
-      <br />
-
       <div>
-        Enter your Name, Registration No. & EmailID and press 'Signup' to enter user dashboard
+        <form onSubmit={handleVerify} class="bg-white p-6 rounded-lg shadow-md">
+          <div class="mb-4">
+            <label class="block text-gray-700 font-medium mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              class="w-full border border-gray-400 p-2"
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+
+          <button class="bg-indigo-500 text-white py-2 px-4 rounded-full hover:bg-indigo-600">
+            Verify
+          </button>
+        </form>
+
+        <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-md">
+          <div className="mb-4">
+            <label className="block text-gray-700 font-medium mb-2" htmlFor="contactNumber">
+              Contact Number
+            </label>
+            <input
+              className="w-full border border-gray-400 p-2"
+              type="text"
+              id="contactNumber"
+              name="contactNumber"
+              value={contactNumber}
+              onChange={(event) => setContactNumber(event.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-medium mb-2" htmlFor="otp">
+              OTP
+            </label>
+            <input
+              className="w-full border border-gray-400 p-2"
+              type="text"
+              id="otp"
+              name="otp"
+              value={otp}
+              onChange={(event) => setOtp(event.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-medium mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="w-full border border-gray-400 p-2"
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-medium mb-2" htmlFor="regNumber">
+              Registration Number
+            </label>
+            <input
+              className="w-full border border-gray-400 p-2"
+              type="text"
+              id="regNumber"
+              name="regNumber"
+              value={regNumber}
+              onChange={(event) => setRegNumber(event.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-medium mb-2" htmlFor="username">
+              User Name
+            </label>
+            <input
+              className="w-full border border-gray-400 p-2"
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <button className="bg-indigo-500 text-white py-2 px-4 rounded-full hover:bg-indigo-600">
+            Submit
+          </button>
+        </form>
       </div>
-      <div>{"[ Input field for name ]"}</div>
-      <div>{"[ Input field for Reg No. ]"}</div>
-      <div>{"[ Input field for Email ID ]"}</div>
-      <div>{"[ 'Signup' Button(opens user dashboard) ]"}</div>
     </div>
   );
 }
