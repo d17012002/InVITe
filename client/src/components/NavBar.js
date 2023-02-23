5c551556456abf1dfca6631b904a392141b1880f
Saksham Gupta <saksham.gupta2020@vitbhopal.ac.in>
2023-02-23 15:43:05 +0530

    navbar changes

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,24 +1,50 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FaAngleDown } from "react-icons/fa";
 
 export default function NavBar() {
     const router = useRouter();
     const [showDropdown, setShowDropdown] = useState(false);
+    const dropdownRef = useRef(null);
 
     const toggleDropdown = () => {
         setShowDropdown(!showDropdown);
     };
 
+    // Attaches an event listener for the 'mousedown' event to detect a click outside the dropdown
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                dropdownRef.current &&
+                !dropdownRef.current.contains(event.target)
+            ) {
+                setShowDropdown(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [dropdownRef]);
+
     return (
-        <header className="bg-[color:var(--white-color)] fixed top-0 z-50 w-full shadow-md text-[color:var(--secondary-color)]">
+        <header className="bg-[color:var(--white-color)] fixed top-0 z-50 w-full shadow-md text-[color:var(--darker-secondary-color)]">
             <div className="container mx-auto flex items-center flex-col lg:flex-row justify-between p-4">
-                <div className="flex items-center">
-                    {/* <img src="/logo.png" alt="Logo" className="h-8" /> */}
-                    <h1
-                        onClick={() => router.push("/users/dashboard")}
-                        className="m-2 text-gray-800 font-bold text-4xl cursor-pointer"
-                    >
+                <div
+                    onClick={() => router.push("/users/dashboard")}
+                    className="flex items-center gap-x-3 cursor-pointer"
+                >
+                    <Image
+                        src="/favicon_io/android-chrome-192x192.png"
+                        width={500}
+                        height={500}
+                        alt="Logo"
+                        className="h-8 w-8"
+                    />
+                    <h1 className="m-2 text-gray-800 font-bold text-4xl">
                         In
                         <span className="text-[color:var(--secondary-color)]">
                             VIT
@@ -46,12 +72,15 @@ export default function NavBar() {
                         >
                             <a>Team</a>
                         </li>
-                        <li className="mr-4 cursor-pointer">
+                        <li
+                            className="mr-4 cursor-pointer relative"
+                            ref={dropdownRef}
+                        >
                             <button
                                 onClick={toggleDropdown}
                                 className="flex items-center justify-center bg-[color:var(--darker-secondary-color)] text-white text-sm font-medium rounded-md w-28 h-10 focus:outline-none"
                             >
-                                <span className="mr-2">Username</span>
+                                <span className="mr-2">Profile</span>
                                 <FaAngleDown
                                     className={`h-4 w-4 ${
                                         showDropdown
@@ -61,7 +90,7 @@ export default function NavBar() {
                                 />
                             </button>
                             {showDropdown && (
-                                <div className="absolute mt-2 w-56 bg-white rounded-md overflow-hidden shadow-lg z-10">
+                                <div className="absolute right-0 mt-2 w-56 bg-white rounded-md overflow-hidden shadow-lg z-10">
                                     <div className="px-4 py-2">
                                         <div className="text-gray-800 font-medium">
                                             Full Name
--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -1,15 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-      <Html lang="en">
-          <Head>
-              <link rel="shortcut icon" href="/favicon_io/favicon.ico" />
-          </Head>
-          <body>
-              <Main />
-              <NextScript />
-          </body>
-      </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head>
+                <link rel="shortcut icon" href="/favicon_io/favicon.ico" />
+                <title>InVITe</title>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
