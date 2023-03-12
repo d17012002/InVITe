5fec323b8edf23e2ab0db872a1e5fe6b5c1bebbc
Anurag Kumar <2002anuragksingh@gmail.com>
2023-03-12 16:55:29 +0530

    delete event api

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { postEvent, allEvents, particularEvent } = require("../controllers/eventController");
+const { postEvent, allEvents, particularEvent, deleteEvent } = require("../controllers/eventController");
 
 router.route("/post/event").post(postEvent);
 router.route("/getallevents").get(allEvents);
 router.route("/getevent").post(particularEvent);
+router.route("/deleteevent").post(deleteEvent);
 
 
 module.exports = router;
